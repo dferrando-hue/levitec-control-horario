c6d4b47a2b6a7c4b3116eceea3de2fb3fbc5a841
dferrando-hue <dferrando@levitec.es>
2026-09-25 12:31:45 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='levitec-fichaje-v1-2';
+const CACHE='levitec-fichaje-v2a';
 const SHELL=['./','./index.html','./manifest.webmanifest','./levitec-fichaje-192.png','./levitec-fichaje-512.png'];
 
 self.addEventListener('install', event => {
